2b38ef2c3162614b78e02905b47239c16a6897cc
Jiro <j.code.public@gmail.com>
2025-03-14 13:21:16 -0700

    Add main pkg

---
+++ errors_test.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	 "github.com/gooolib/testing/table"
+)
+
+func TestErrors(t *testing.T) {
+	err := New("msg")
+
+	test := table.NewTable([]table.Record[string, []string]{
+		{
+			Name: "Stacktrace",
+			Subject: func(_t *testing.T) (string, error) {
+				return err.StackTrace(), nil
+			},
+			Expect: func(t *testing.T) ([]string, error) {
+				return []string{
+					"gooolib/errors/errors_test.go. method: TestErrors. line: 12",
+					"src/testing/testing.go. method: tRunner. line: 1689",
+					"src/runtime/asm_amd64.s. method: goexit. line: 1695",
+					"",
+				}, nil
+			},
+			Assert: func(t *testing.T, r *table.Record[string, []string]) bool {
+				e, _ := r.Expect(t)
+				s, _ := r.Subject(t)
+				lines := strings.Split(s, "\n")
+				for i, line := range lines {
+					if !strings.HasSuffix(line, e[i]) {
+						t.Errorf("Expected(line %d) %s to contain %s", i, line, e[i])
+						return false
+					}
+				}
+				return true
+			},
+		},
+		{
+			Name: "Print Error with +v",
+			Subject: func(_t *testing.T) (string, error) {
+				return fmt.Sprintf("%+v", err), nil
+			},
+			Expect: func(t *testing.T) ([]string, error) {
+				return []string{
+					"pkg/errors : msg",
+					"",
+					"gooolib/errors/errors_test.go. method: TestErrors. line: 12",
+					"src/testing/testing.go. method: tRunner. line: 1689",
+					"src/runtime/asm_amd64.s. method: goexit. line: 1695",
+					"",
+					"",
+				}, nil
+			},
+			Assert: func(t *testing.T, r *table.Record[string, []string]) bool {
+				e, _ := r.Expect(t)
+				s, _ := r.Subject(t)
+				lines := strings.Split(s, "\n")
+				for i, line := range lines {
+					if !strings.HasSuffix(line, e[i]) {
+						t.Errorf("Expected(line %d) %s to contain %s", i, line, e[i])
+						return false
+					}
+				}
+				return true
+			},
+		},
+		{
+			Name: "Print Error with v",
+			Subject: func(_t *testing.T) (string, error) {
+				return fmt.Sprintf("%v", err), nil
+			},
+			Expect: func(t *testing.T) ([]string, error) {
+				return []string{"pkg/errors : msg"}, nil
+			},
+			Assert: func(t *testing.T, r *table.Record[string, []string]) bool {
+				e, _ := r.Expect(t)
+				s, _ := r.Subject(t)
+				return s == e[0]
+			},
+		},
+		{
+			Name: "Print Error with s",
+			Subject: func(_t *testing.T) (string, error) {
+				return fmt.Sprintf("%s", err), nil
+			},
+			Expect: func(t *testing.T) ([]string, error) {
+				return []string{"pkg/errors : msg"}, nil
+			},
+			Assert: func(t *testing.T, r *table.Record[string, []string]) bool {
+				e, _ := r.Expect(t)
+				s, _ := r.Subject(t)
+				return s == e[0]
+			},
+		},
+	})
+
+	test.Run(t)
+}
+
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gooolib/errors
+
+go 1.22.3
+
+require github.com/gooolib/testing v0.0.0-20250314201531-08ff6e6b2f90
